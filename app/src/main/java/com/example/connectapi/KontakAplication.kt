0650f6c193200bba2a386b8e92053485c59aaaf4
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-26 21:00:27 +0700

    Membuat fungsi TopAppBarKontak

--- app/src/main/java/com/example/connectapi/KontakAplication.kt
+++ app/src/main/java/com/example/connectapi/KontakAplication.kt
@@ -1,7 +1,13 @@
 package com.example.connectapi
 
 import android.app.Application
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,8 +26,23 @@ class KontakAplication : Application() {
 @Composable
 fun TopAppBarKontak(
     title: String,
-    canNavigateBAck: Boolean,
+    canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-){}
+){
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
+        )
+}
