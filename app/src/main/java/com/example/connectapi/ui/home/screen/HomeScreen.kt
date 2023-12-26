d4b3260d246c4ace4c48d097534b3d8e0aad305c
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-26 21:22:26 +0700

    Menambah isi HomeScreen

--- app/src/main/java/com/example/connectapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/connectapi/ui/home/screen/HomeScreen.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -95,6 +97,7 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier) {
 @Composable
 fun KontakCard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     Card(
@@ -108,6 +111,7 @@ fun KontakCard(
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = kontak.nama,
@@ -127,6 +131,11 @@ fun KontakCard(
                 text = kontak.alamat,
                 style = MaterialTheme.typography.titleMedium
             )
+            Spacer(Modifier.weight(1f))
+            IconButton(onClick = {onDeleteClick(kontak)}) {
+                Icon(imageVector = Icons.Default.Delete,
+                    contentDescription = "")
+            }
         }
 
     }
