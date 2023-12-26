f3c7b1007ad991c069ff5890a5caef86c099e8c9
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-26 23:00:31 +0700

    Membuat fungsi FormInputSiswa + memebuat parameternya

--- app/src/main/java/com/example/connectapi/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/connectapi/ui/kontak/screen/InsertScreen.kt
@@ -1,2 +1,13 @@
 package com.example.connectapi.ui.kontak.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.connectapi.ui.kontak.viewmodel.InsertUiEvent
+
+@Composable
+fun FormInputSiswa(
+  insertUiEvent: InsertUiEvent,
+  modifier: Modifier = Modifier,
+  onValueChange: (InsertUiEvent) -> Unit = {},
+  enabled: Boolean = true
+){}
