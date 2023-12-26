92e06de39ba9943e2b6e3924672bbf5d8f7dc276
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2023-12-26 21:29:04 +0700

    Menambah parameter HomeStatus

--- app/src/main/java/com/example/connectapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/connectapi/ui/home/screen/HomeScreen.kt
@@ -37,7 +37,9 @@ import com.example.connectapi.ui.home.viewmodel.KontakUIState
 fun HomeStatus(
     kontakUIState: KontakUIState,
     retryAction: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Kontak) -> Unit = {},
+    onDetailClick: (Kontak) -> Unit
 ) {
 
     when (kontakUIState) {
